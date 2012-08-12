5debce016d4c89b60c8c309154e2a0a52776d092
Liu Bing <liubinging@gmail.com>
2012-08-12 21:40:04 +0800

    Changed the resources geting methord and url format of update.java

--- src/com/iBeiKe/InfoPortal/update/Update.java
+++ src/com/iBeiKe/InfoPortal/update/Update.java
@@ -5,34 +5,57 @@ import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Environment;
+import android.util.Log;
 
+import com.iBeiKe.InfoPortal.R;
 import com.iBeiKe.InfoPortal.common.Archive;
 import com.iBeiKe.InfoPortal.common.Files;
 
 public class Update implements Runnable {
-	public Update() {
+	Context context;
+	public Update(Context context) {
 		Thread th = new Thread(this);
 		th.start();
+		this.context = context;
 	}
-	public void get() throws IOException {
-		String path = Environment.getExternalStorageDirectory().getPath();
+	public void get() throws IOException, NameNotFoundException {
+		String SDpath = Environment.getExternalStorageDirectory().getPath();
+		PackageManager pm = context.getPackageManager();
+		PackageInfo pi = pm.getPackageInfo(context.getPackageName(), 0);
+		int version = pi.versionCode;
 		InputStream stream = null;
-		URL url = new URL("http://m.unixoss.com/infoportal.zip");
+		URL url = new URL("http://mobil.unixoss.com/?ver=" + version);
 		HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+		for(int i=0;;i++) {
+			String key = connection.getHeaderFieldKey(i);
+			String mine = connection.getHeaderField(i); 
+			Log.d(i + ". " + key, mine + "");
+			if(mine==null) break;
+		    if("content-disposition".equals(connection.getHeaderFieldKey(i).toLowerCase())) {
+			    Log.d("filename", mine);
+		    }
+		}
 		stream = connection.getInputStream();
-		Files file = new Files(path + "/infoportal/tmp/db.zip");
+		Files file = new Files(SDpath +
+				context.getResources().getString(R.string.path_download));
 		file.saveStream(stream);
 		stream.close();
 		Archive arc = new Archive();
-		arc.unzip(file.getPath(), "/data/data/" +
-				"com.iBeiKe.InfoPortal/databases/infoportal.db");
+		arc.unzip(file.getPath(), 
+				context.getResources().getString(R.string.path_database));
 	}
 
 	public void run() {
 		try {
 			get();
 		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (NameNotFoundException e) {
 			e.printStackTrace();
 		}
 	}

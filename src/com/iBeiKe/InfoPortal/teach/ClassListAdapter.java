ffe5fbb36a81d205a88b47c51c86a65db32ff5d2
Liu Bing <liubinging@gmail.com>
2012-09-16 19:55:37 +0800

    Added new classes list adapter to support the list view displayment.

---
+++ src/com/iBeiKe/InfoPortal/teach/ClassListAdapter.java
@@ -0,0 +1,36 @@
+package com.iBeiKe.InfoPortal.teach;
+
+import android.app.Activity;
+import android.content.Context;
+import android.database.Cursor;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+public class ClassListAdapter extends BaseAdapter {
+	private Activity activity;
+	
+	public ClassListAdapter(Activity activity) {
+		this.activity = activity;
+	}
+
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public Object getItem(int arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public long getItemId(int position) {
+		// TODO Auto-generated method stub
+		return position;
+	}
+
+	public View getView(int position, View convertView, ViewGroup parent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}

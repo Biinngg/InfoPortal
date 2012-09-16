61261d395d5fb5f93cc09a3063e0f283dcfb6e90
Liu Bing <liubinging@gmail.com>
2012-09-16 19:44:49 +0800

    Added the base adapter helper which is an abstract class to provide the list view database data.

---
+++ src/com/iBeiKe/InfoPortal/database/BaseAdapterHelper.java
@@ -0,0 +1,11 @@
+package com.iBeiKe.InfoPortal.database;
+
+import android.app.Activity;
+
+public class BaseAdapterHelper {
+	private Activity activity;
+	
+	public BaseAdapterHelper(Activity avtivity) {
+		this.activity = activity;
+	}
+}
